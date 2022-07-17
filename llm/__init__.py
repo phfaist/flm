d551bdf2490ea88ef9369d3a2b792ad0579d19f8
Philippe Faist <phfaist@gmail.com>
2022-07-18 00:02:20 +0200

    bump version

--- llm/__init__.py
+++ llm/__init__.py
@@ -3,4 +3,4 @@
 # Version is hard-coded here to avoid any other dependencies (no importlib
 # magic, no pkg_resources, no other stuff like that), especially for possible
 # future use with transcrypt.
-__version__ = "0.1.0beta0"
+__version__ = "0.1.0beta1"
