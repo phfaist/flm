b3f994f548e2192dcb0a2604d7778299746e4fd7
Philippe Faist <phfaist@gmail.com>
2022-09-27 20:42:55 +0200

    v bump

--- llm/__init__.py
+++ llm/__init__.py
@@ -3,6 +3,6 @@
 # Version is hard-coded here to avoid any other dependencies (no importlib
 # magic, no pkg_resources, no other stuff like that), especially for possible
 # future use with transcrypt.
-__version__ = "0.2.0alpha000005"
+__version__ = "0.2.0alpha000006"
 
 # ALSO BUMP VERSION NUMBER IN pyproject.toml !!
