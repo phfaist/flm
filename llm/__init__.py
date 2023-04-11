bf203dae6dc104b0bf54be35db0b857dd4cacd12
Philippe Faist <phfaist@gmail.com>
2023-04-11 17:25:38 +0200

    tweaks

--- llm/__init__.py
+++ llm/__init__.py
@@ -3,6 +3,6 @@
 # Version is hard-coded here to avoid any other dependencies (no importlib
 # magic, no pkg_resources, no other stuff like that), especially for possible
 # future use with transcrypt.
-__version__ = "0.3.0alpha000007"
+__version__ = "0.3.0alpha000008"
 
 # ALSO BUMP VERSION NUMBER IN pyproject.toml !!
