445ea9232e29ebddb4cb81498b5742dab848f5a9
Philippe Faist <phfaist@gmail.com>
2024-07-17 00:58:53 +0200

    v bump

--- flm/__init__.py
+++ flm/__init__.py
@@ -3,6 +3,6 @@
 # Version is hard-coded here to avoid any other dependencies (no importlib
 # magic, no pkg_resources, no other stuff like that), especially for possible
 # future use with transcrypt.
-__version__ = "0.3.0alpha000014"
+__version__ = "0.3.0alpha000015"
 
 # ALSO BUMP VERSION NUMBER IN pyproject.toml !!
